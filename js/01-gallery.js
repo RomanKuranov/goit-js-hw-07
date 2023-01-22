66e0b3d690cc3f1c843fec56e7f3c79d7a3abb7a
RomanKuranov <romankuranov93@gmail.com>
2023-01-22 20:38:12 +0200

    task-1 without ESC

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const divEl = document.querySelector(".gallery");
+
+const creatgalleryItems = (galleryItems) => {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+    </div>`;
+    }).join('');
+};
+const galleryMarkup = divEl.insertAdjacentHTML('beforeend', creatgalleryItems(galleryItems));
+
+
+const onDivElClick = (event) => {
+    event.preventDefault()
+    const largeImage = event.target.dataset.source;
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${largeImage}">
+`);
+instance.show()
+};
+
+divEl.addEventListener('click', onDivElClick );
+
